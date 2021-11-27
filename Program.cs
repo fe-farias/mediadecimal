8f85a8feaec4e3a7c593c03dff38db20d2b81515
Felipe Francisco <fefarias97@gmail.com>
2021-11-27 16:52:15 -0300

    Resolução exercicio média decimal

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int quantidade,contador=1;
+double n,soma=0, media=0, maior=0, menor=0;
+
+
+
+Console.Write("Quantos Números: ");
+quantidade = Convert.ToInt32(Console.ReadLine());
+
+while(contador<=quantidade){
+    Console.Write($"Número #{contador}: ");
+    n = Convert.ToDouble(Console.ReadLine());
+    if (contador == 1){
+        maior = n;
+        menor = n;
+    }else if (n>maior){
+        maior = n;
+    }else if(n<menor){
+        menor = n;
+    }
+    contador+=1;
+    soma+=n;
+    media = soma/quantidade;
+}
+
+Console.WriteLine($"A soma é.... {soma:N} ");
+Console.WriteLine($"A média é... {media:N} ");
+Console.WriteLine($"O maior número é {maior:N}");
+Console.WriteLine($"O menor número é {menor:N}");
